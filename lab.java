1eddf06a370ee240ee5049238527156bd82d9aee
Zenith <thunderzen01@gmail.com>
2026-05-18 19:34:38 +0545

    Lab2

--- lab.java
+++ lab.java
@@ -1,36 +1,24 @@
-import java.util.Scanner;
-
-class Student {
-    int roll;
-    String name;
-
-    Student() {
-        roll = 0;
-        name = null;
-    }
-
-    Student(int r, String n) {
-        roll = r;
-        name = n;
-    }
-
-    void display() {
-        System.out.println("Roll: " + roll);
-        System.out.println("Name: " + name);
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
     }
 }
 
-class Main {
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+}
+
+class lab2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter roll number: ");
-        int r = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter name: ");
-        String n = sc.nextLine();
-
-        Student s1 = new Student(r, n);
-        s1.display();
-
+        Dog a1 = new Dog();
+        Cat a2 = new Cat();
+        a1.sound();
+        a2.sound();
     }
 }
