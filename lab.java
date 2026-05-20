9b201b8f93c43e826b111fa1f2aaca6d41abeee9
Zenith <thunderzen01@gmail.com>
2026-05-20 16:15:35 +0545

    Lab3

--- lab.java
+++ lab.java
@@ -1,24 +1,109 @@
-class Animal {
-    void sound() {
-        System.out.println("Animal makes a sound");
+import java.util.ArrayList;
+import java.util.Scanner;
+
+abstract class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    abstract void borrowBook(String book);
+
+    void calculateFee() {
+        System.out.println("Fee = 2000");
+    }
+
+    void calculateGrade() {
+
+        if (marks >= 80)
+            System.out.println("Grade = A");
+
+        else if (marks >= 60)
+            System.out.println("Grade = B");
+
+        else
+            System.out.println("Grade = C");
     }
 }
 
-class Dog extends Animal {
+interface Library {
+    void showBorrowedBooks();
+}
+
+class LibraryStudent extends Student implements Library {
+
+    ArrayList<String> borrowedBooks = new ArrayList<>();
+
+    LibraryStudent(String name, int marks) {
+        super(name, marks);
+    }
+
     @Override
-    void sound() {
-        System.out.println("Dog barks");
+    void borrowBook(String book) {
+        borrowedBooks.add(book);
+    }
+
+    @Override
+    public void showBorrowedBooks() {
+        System.out.println("Borrowed Books: " + borrowedBooks);
     }
 }
 
-class Cat extends Animal {
-}
+class lab {
 
-class lab2 {
+    static ArrayList<String> getLibraryBooks() {
+
+        ArrayList<String> books = new ArrayList<>();
+
+        books.add("Java");
+        books.add("C");
+        books.add("Python");
+
+        return books;
+    }
+
     public static void main(String[] args) {
-        Dog a1 = new Dog();
-        Cat a2 = new Cat();
-        a1.sound();
-        a2.sound();
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> libraryBooks = getLibraryBooks();
+
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+        sc.nextLine();
+
+        LibraryStudent s1 = new LibraryStudent(name, marks);
+
+        System.out.println("Library Books: " + libraryBooks);
+
+        System.out.print("Enter book to borrow: ");
+        String book = sc.nextLine();
+
+        if (libraryBooks.contains(book)) {
+
+            libraryBooks.remove(book);
+
+            s1.borrowBook(book);
+
+            System.out.println("Book borrowed successfully.");
+        }
+
+        else {
+            System.out.println("Book not available.");
+        }
+
+        System.out.println("\nStudent Name: " + name);
+
+        s1.calculateFee();
+        s1.calculateGrade();
+        s1.showBorrowedBooks();
+
+        sc.close();
     }
 }
